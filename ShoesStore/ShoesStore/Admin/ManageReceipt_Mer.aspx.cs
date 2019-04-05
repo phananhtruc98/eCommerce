d90334c5e642cf83b52683cd2a6c97789e40d2f0
Truc <anhtrucphanit@gmail.com>
2019-04-05 12:45:28 +0700

    thêm xóa sửa rcpt_sub

--- ShoesStore/ShoesStore/Admin/ManageReceipt_Mer.aspx.cs
+++ ShoesStore/ShoesStore/Admin/ManageReceipt_Mer.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using ShoesStore.BusinessLogicLayer;
+using ShoesStore.DataAccessLogicLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,12 @@ namespace ShoesStore.Admin
     public partial class ManageReceipt_Mer : System.Web.UI.Page
     {
         private readonly RcptSub_BUS rcptSub_BUS = new RcptSub_BUS();
+        private readonly Rcpt_BUS rcpt_BUS = new Rcpt_BUS();
         protected void Page_Load(object sender, EventArgs e)
         {
-            BindDataGridView();
+            BindGridViewData();
         }
-        private void BindDataGridView()
+        private void BindGridViewData()
         {
             gvRcptSub.DataSource = (from rcptSub in rcptSub_BUS.GetAll_Join_Rcpt()
                                     select new
@@ -27,6 +29,95 @@ namespace ShoesStore.Admin
                                         rcptSub.Rcpt.UsrEdit
                                     }).ToList();
             gvRcptSub.DataBind();
+        }
+
+        protected void gvSub_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "EditRow")
+            {
+                int rowIndex = ((GridViewRow)((LinkButton)e.CommandSource).NamingContainer).RowIndex;
+                gvRcptSub.EditIndex = rowIndex;
+                BindGridViewData();
+            }
+            else if (e.CommandName == "DeleteRow")
+            {
+                RcptSub result = (from c in rcptSub_BUS.GetAll_Join_Rcpt()
+                              where c.RcptSubId == Convert.ToInt32(e.CommandArgument)
+                              select c).FirstOrDefault();
+                Rcpt result1 = (from c in rcpt_BUS.GetAll()
+                                  where c.RcptId == Convert.ToInt32(e.CommandArgument)
+                                  select c).FirstOrDefault();
+                rcptSub_BUS.Delete(result);
+                rcpt_BUS.Delete(result1);
+                BindGridViewData();
+            }
+            else if (e.CommandName == "CancelUpdate")
+            {
+                gvRcptSub.EditIndex = -1;
+                BindGridViewData();
+            }
+            else if (e.CommandName == "UpdateRow")
+            {
+                int rowIndex = ((GridViewRow)((LinkButton)e.CommandSource).NamingContainer).RowIndex;
+                int RcptSubId = Convert.ToInt32(e.CommandArgument);
+                string rcptSubId = ((TextBox)gvRcptSub.Rows[rowIndex].FindControl("EditRcptSubId")).Text;
+                string DateAdd = ((TextBox)gvRcptSub.Rows[rowIndex].FindControl("EditDateAdd")).Text;
+                string DateEdit = ((TextBox)gvRcptSub.Rows[rowIndex].FindControl("EditDateEdit")).Text;
+                string UsrAdd = ((TextBox)gvRcptSub.Rows[rowIndex].FindControl("EditUsrAdd")).Text;
+                string UsrEdit = ((TextBox)gvRcptSub.Rows[rowIndex].FindControl("EditUsrEdit")).Text;
+                // SỬA CODE Ở ĐÂY
+                RcptSub result = (from c in rcptSub_BUS.GetAll()
+                              where c.RcptSubId == Convert.ToInt32(e.CommandArgument)
+                              select c).FirstOrDefault();
+                if (result != null)
+                {
+                    // SỬA CODE Ở ĐÂY
+                    result.RcptSubId = RcptSubId;
+                    result.Rcpt.DateAdd = Convert.ToDateTime(DateAdd);
+                    result.Rcpt.DateEdit = Convert.ToDateTime(DateEdit);
+                    result.Rcpt.UsrAdd = Convert.ToInt32(UsrAdd);
+                    result.Rcpt.UsrEdit = Convert.ToInt32(UsrEdit);
+                    rcptSub_BUS.Update(result);
+                }
+
+                gvRcptSub.EditIndex = -1;
+                BindGridViewData();
+            }
+            else if (e.CommandName == "InsertRow")
+            {
+                // SỬA CODE Ở ĐÂY
+                string rcptSubId = ((TextBox)gvRcptSub.FooterRow.FindControl("InsertSupId")).Text;
+                string dateAdd = ((TextBox)gvRcptSub.FooterRow.FindControl("InsertDateAdd")).Text;
+                string dateEdit = ((TextBox)gvRcptSub.FooterRow.FindControl("InsertDateEdit")).Text;
+                string usrAdd = ((TextBox)gvRcptSub.FooterRow.FindControl("InsertUsrAdd")).Text;
+                string usrEdit = ((TextBox)gvRcptSub.FooterRow.FindControl("InsertUsrEdit")).Text;
+                if (dateAdd == "" || usrAdd == "")
+                {
+                    return;
+                }
+
+
+                Rcpt rcpt = new Rcpt
+                {
+                    RcptId = Convert.ToInt32(rcptSubId),
+                    DateAdd = Convert.ToDateTime(dateAdd),
+                    DateEdit = Convert.ToDateTime(dateEdit),
+                    UsrAdd = Convert.ToInt32(usrAdd),
+                    UsrEdit = Convert.ToInt32(usrEdit)
+                };
+
+                RcptSub newRcptSub = new RcptSub {
+                    RcptSubId = Convert.ToInt32(rcptSubId)
+                };
+
+                
+
+                rcptSub_BUS.Insert(newRcptSub);
+                rcpt_BUS.Insert(rcpt);
+
+
+                BindGridViewData();
+            }
         }
     }
 }
