96893b6c033ae8759e95cf493bebda7b8b3dd120
Truc <anhtrucphanit@gmail.com>
2019-04-18 17:47:37 +0700

    thêm mấy số trang

---
+++ ShoesStore/ShoesStore/Customer/CusHome_ChangePassword.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShoesStore.Customer
+{
+    public partial class CusHome_ChangePassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lbtnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lbtnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
