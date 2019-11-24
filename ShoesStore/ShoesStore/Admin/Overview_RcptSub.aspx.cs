b2a2161ddddee329eb8a807e721191d5abbf319c
Truc Phan <anhtrucphanit@gmail.com>
2019-11-24 16:29:22 +0700

    Add chart into excel [Admin]

--- ShoesStore/ShoesStore/Admin/Overview_RcptSub.aspx.cs
+++ ShoesStore/ShoesStore/Admin/Overview_RcptSub.aspx.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Drawing.Chart;
+using System;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Utilities;
@@ -111,16 +115,53 @@ namespace ShoesStore.Admin
             FileHelper helper = new FileHelper();
             var rs = MyLibrary.Shp_Bus.GetAll().ToList();
             DataTable table = new DataTable();
-            table.Columns.Add("Mã cửa hàng");
-            table.Columns.Add("Tên cửa hàng");
-            table.Columns.Add("Tổng thu");
-            table.Columns.Add("Tổng chi");
+            table.Columns.Add("Mã cửa hàng", typeof(string));
+            table.Columns.Add("Tên cửa hàng", typeof(string));
+            table.Columns.Add("Tổng thu", typeof(int));
+            table.Columns.Add("Tổng chi", typeof(int));
             foreach (var item in rs)
             {
                 table.Rows.Add(item.ShpId, item.ShpName, MyLibrary.SumIn(item), MyLibrary.SumOut(item));
             }
             table.AcceptChanges();
-            helper.ExportExcel(table, "Thống kê thu chi của Merchant");
+            //helper.ExportExcel(table, "Thống kê thu chi của Merchant");
+            ExcelPackage excel = new ExcelPackage();
+            var workSheet = excel.Workbook.Worksheets.Add("Thống kê thu chi của Merchant");
+            for (int i = 1; i < table.Columns.Count + 1; i++)
+            {
+                workSheet.Cells[1, i].Style.Font.Bold = true;
+                workSheet.Column(i).AutoFit();
+            }
+            // Create chart
+            ExcelChart chart = workSheet.Drawings.AddChart("chart", eChartType.ColumnClustered); //To add chart to added sheet of type column clustered chart  
+            chart.XAxis.Title.Text = "Tên cửa hàng"; //give label to x-axis of chart  
+            chart.XAxis.Title.Font.Size = 10;
+            chart.YAxis.Title.Text = "đồng"; //give label to Y-axis of chart  
+            chart.YAxis.Title.Font.Size = 10;
+            chart.SetSize(1200, 300);
+            chart.SetPosition(1, 0, 6, 0);
+            chart.YAxis.MinValue = 39;
+            var row = 1;
+            var tongThu = chart.Series.Add(("C" + (row + 1) + ":" + "C" + (table.Rows.Count + 1)), ("B" + (row + 1) + ":" + "B" + (table.Rows.Count + 1)));
+            tongThu.Header = "Tổng thu";
+            var tongChi = chart.Series.Add(("D" + (row + 1) + ":" + "D" + (table.Rows.Count + 1)), ("B" + (row + 1) + ":" + "B" + (table.Rows.Count + 1)));
+            tongChi.Header = "Tổng chi";
+
+            workSheet.Cells[1, 1].LoadFromDataTable(table, true);
+            //
+
+
+            HttpResponse response = HttpContext.Current.Response;
+            using (var memoryStream = new MemoryStream())
+            {
+                response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                response.AddHeader("content-disposition", "attachment;  filename=Thống kê thu chi của Merchant.xlsx");
+                excel.SaveAs(memoryStream);
+                memoryStream.WriteTo(response.OutputStream);
+                response.Flush();
+                response.End();
+                excel.Dispose(); //will dispose the package  
+            }
         }
     }
 }   
